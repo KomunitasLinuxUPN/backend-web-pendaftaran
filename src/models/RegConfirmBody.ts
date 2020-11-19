bf1ae71738a21e7801002abecbd4b5fa8cacf116
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-11-19 17:20:21 +0700

    🛠 Working code

--- src/models/RegConfirmBody.ts
+++
@@ -1,4 +0,0 @@
-export default interface ConfirmationBody {
-  token: string
-  email: string
-}
--- src/routes/confirmation.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as confirmationController from '../controllers/confirmation'
-
-const router = express.Router()
-
-router.post('/confirm-registration', confirmationController.confirmRegistration)
-
-export default router

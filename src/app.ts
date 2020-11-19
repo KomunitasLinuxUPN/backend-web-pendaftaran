55d9c1ffc12be6bb9c1bdf95b5bbc8bf23219126
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-11-19 15:53:54 +0700

    ⛑ Add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
 
 import homeRoutes from './routes/home'
 import confirmationRoutes from './routes/confirmation'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(helmet())
 
 app.use(homeRoutes)
 app.use(confirmationRoutes)
